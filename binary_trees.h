fea3f335e511126fd6f0dd34573432c85ad0e874
DennisWanjeri <shikandadennis07@gmail.com>
2021-07-19 12:51:38 +0000

    task 1 and 2

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef BINARY_TREES
 #define BINARY_TREES
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -44,4 +47,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+/*provided*/
+void binary_tree_print(const binary_tree_t *tree);
 #endif /*BINARY_TREES*/
